708921701a4e0b2d6abb63b63209ecab644ce60c
Samuel Montealegre <montealegreq-samuel@javeriana.edu.co>
2026-02-19 15:01:48 -0500

    organiza y documenta el  WikiController

--- src/main/java/co/javeriana/dw/thymeleaf/controller/WikiController.java
+++ src/main/java/co/javeriana/dw/thymeleaf/controller/WikiController.java
@@ -3,36 +3,35 @@ package co.javeriana.dw.thymeleaf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Controldor principal de la Wiki.
+ * Esto maneja las rutas generales del proyecto.
+ */
 @Controller
 public class WikiController {
-    
+
+    // La pagina principal
     @GetMapping("/")
     public String home() {
         return "index";
     }
-    
-    @GetMapping("/wiki")
-    public String wiki() {
-        return "index";
-    }
-    
-    @GetMapping("/home")
-    public String showHome() {
-        return "index";
-    }
-    
-    @GetMapping("/sections")
-    public String sections() {
-        return "index";
-    }
-    
+
+    // Sección de documentacion
     @GetMapping("/documentation")
     public String documentation() {
         return "index";
     }
-    
+
+    // Sección acerca del proyecto
     @GetMapping("/about")
     public String about() {
         return "index";
     }
+
+    // Sección de características
+    @GetMapping("/features")
+    public String features() {
+        return "index";
+    }
+
 }
